8d11213da1b1e774487955e683111dc2b6c48e4b
rohit-sharma77 <rp.rohit@gmail.com>
2018-01-12 20:14:31 -0500

    Added additional unit tests.

--- TwitterFun.Tests/Controllers/HomeControllerTest.cs
+++ TwitterFun.Tests/Controllers/HomeControllerTest.cs
@@ -13,7 +13,7 @@ namespace TwitterFun.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void TestIndexPageSuccess()
         {
             // Arrange
             HomeController controller = new HomeController();
@@ -26,7 +26,7 @@ namespace TwitterFun.Tests.Controllers
         }
 
         [TestMethod]
-        public void About()
+        public void TestAboutPageSuccess()
         {
             // Arrange
             HomeController controller = new HomeController();
@@ -39,14 +39,61 @@ namespace TwitterFun.Tests.Controllers
         }
 
         [TestMethod]
-        public void GetTenRecordsForSalesForce()
+        [ExpectedException(typeof(Exception), "No URL provided for authentication")]
+        public void AuthUrlMissingFailTest()
         {
             // Arrange
             HomeController controller = new HomeController();
             // Act
             JsonResult result = controller.GetDetails(10, "Salesforce");
-            Assert.Fail("No URL provided for authentication");
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception), "Error while getting the access token")]
+        public void GetTenTweetsFailureForIncorrectCreds()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            System.Web.Configuration.WebConfigurationManager.AppSettings["AuthUrl"] = "https://api.twitter.com/oauth2/token";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerKey"] = "test";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerSecret"] = "test";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ReadTweetUrl"] = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+            ReadContentController contentController = new ReadContentController();
+            contentController.ReadTwitterContent(10, "Salesforce");
+        }
+
+        [TestMethod]
+        public void GetTenRecordsForSalesForceUserSuccess()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            System.Web.Configuration.WebConfigurationManager.AppSettings["AuthUrl"] = "https://api.twitter.com/oauth2/token";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerKey"] = "LKfy4Pv8AOHEntNBwU567Clzg";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerSecret"] = "NJTlecFoyE89FvZEGcH2hzrwTCddZu4e2EcsbE36rtwK4HpPVO";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ReadTweetUrl"] = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+            ReadContentController contentController = new ReadContentController();
+            string response = contentController.ReadTwitterContent(10, "Salesforce");
+            Assert.IsNotNull(response);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Net.WebException), "The remote server returned an error: (401) Unauthorized.")]
+        public void GetTenTweetsFailsForIncorrectUsername()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            System.Web.Configuration.WebConfigurationManager.AppSettings["AuthUrl"] = "https://api.twitter.com/oauth2/token";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerKey"] = "LKfy4Pv8AOHEntNBwU567Clzg";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ConsumerSecret"] = "NJTlecFoyE89FvZEGcH2hzrwTCddZu4e2EcsbE36rtwK4HpPVO";
+            System.Web.Configuration.WebConfigurationManager.AppSettings["ReadTweetUrl"] = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+            ReadContentController contentController = new ReadContentController();
+            contentController.ReadTwitterContent(10, "Test");
+        }
     }
 }
